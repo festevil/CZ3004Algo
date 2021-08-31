a610f5e3efcc7a45774894fdcf9867fd810f08af
festevil <62298846+festevil@users.noreply.github.com>
2021-08-31 13:15:59 +0800

    Add files via upload

--- AlgorithmCZ3004/src/entities/Robot.java
+++ AlgorithmCZ3004/src/entities/Robot.java
@@ -2,4 +2,221 @@ package entities;
 
 public class Robot {
 
+	//	@formatter:off
+	public static final int NORTH = 0, EAST = 1, SOUTH = 2, WEST = 3;
+	public static final int
+		FRONT_LEFT = 0, FRONT_CENTER = 1, FRONT_RIGHT = 2,
+		MIDDLE_LEFT = 3, MIDDLE_CENTER = 4, MIDDLE_RIGHT = 5,
+		BACK_LEFT = 6, BACK_CENTER = 7, BACK_RIGHT = 8;
+	public enum Rotate { RIGHT, LEFT };
+	/* 6 sensors in total
+	 * TYPE_PLACEMENT_DIRECTION */
+	public static final int
+		S_FL_N = 0, S_FC_N = 1, S_FR_N = 2, S_FR_E = 3,
+		S_BL_W = 4, L_BL_W = 5;
+	//	@formatter:on
+
+	private boolean isRealRun;	// Real Run or Simulation (Whether to send movement command)
+	private Coordinate currPos;	// MIDDLE_CENTER of Robot
+	private int currDir;		// North, South, East, West
+	
+
+	/**
+	 * <tt>Robot</tt> Constructor (Each <tt>Robot</tt> occupies 3 x 3 cells)
+	 * 
+	 * Default Position: y=1 | x=1, Default Direction: EAST.
+	 */
+	public Robot(boolean isRealRun) {
+		this.isRealRun = isRealRun;
+		currPos = new Coordinate(1, 1);
+		currDir = EAST;
+		
+	}
+
+	/**
+	 * <tt>Robot</tt> Constructor with starting position and direction.
+	 * 
+	 * @param startPos
+	 * @param startDir
+	 */
+	public Robot(Coordinate startPos, int startDir, boolean isRealRun) {
+		this.isRealRun = isRealRun;
+
+		int startY = startPos.getY();
+		int startX = startPos.getX();
+		boolean error = false;
+
+		if (startY == 0 || startY == Map.maxY - 1) {
+			error = true;
+		} else if (startX == 0 || startX == Map.maxX - 1) {
+			error = true;
+		}
+
+		if (error == true) {
+			System.out.println("ERROR: Robot(y:" + startPos.getY() + " x:" + startPos.getX()
+					+ ") cannot be initialised outside of map. Default position is assumed.");
+
+			// Default starting position
+			currPos = new Coordinate(1, 1);
+			currDir = EAST;
+		} else {
+			this.currPos = startPos;
+			this.currDir = startDir;
+		}
+
+	}
+
+	/**
+	 * @return Current position of <tt>Robot</tt>.
+	 */
+	public Coordinate getCurrPos() {
+		return currPos;
+	}
+
+	/**
+	 * @return Current direction of <tt>Robot</tt>.
+	 */
+	public int getCurrDir() {
+		return currDir;
+	}
+
+	/**
+	 * @return <tt>Coordinate[]</tt>: All cells that are occupied by the position of <tt>Robot</tt>.
+	 */
+	public Coordinate[] getFootprint() {
+		Coordinate[] robotFootprint = new Coordinate[9];
+		robotFootprint[MIDDLE_CENTER] = currPos;
+
+		switch (currDir) {
+		case NORTH:
+			robotFootprint[FRONT_LEFT] = new Coordinate(currPos.getY() + 1, currPos.getX() - 1);
+			robotFootprint[FRONT_CENTER] = new Coordinate(currPos.getY() + 1, currPos.getX());
+			robotFootprint[FRONT_RIGHT] = new Coordinate(currPos.getY() + 1, currPos.getX() + 1);
+
+			robotFootprint[MIDDLE_LEFT] = new Coordinate(currPos.getY(), currPos.getX() - 1);
+			robotFootprint[MIDDLE_RIGHT] = new Coordinate(currPos.getY(), currPos.getX() + 1);
+
+			robotFootprint[BACK_LEFT] = new Coordinate(currPos.getY() - 1, currPos.getX() - 1);
+			robotFootprint[BACK_CENTER] = new Coordinate(currPos.getY() - 1, currPos.getX());
+			robotFootprint[BACK_RIGHT] = new Coordinate(currPos.getY() - 1, currPos.getX() + 1);
+			return robotFootprint;
+		case SOUTH:
+			robotFootprint[FRONT_LEFT] = new Coordinate(currPos.getY() - 1, currPos.getX() + 1);
+			robotFootprint[FRONT_CENTER] = new Coordinate(currPos.getY() - 1, currPos.getX());
+			robotFootprint[FRONT_RIGHT] = new Coordinate(currPos.getY() - 1, currPos.getX() - 1);
+
+			robotFootprint[MIDDLE_LEFT] = new Coordinate(currPos.getY(), currPos.getX() + 1);
+			robotFootprint[MIDDLE_RIGHT] = new Coordinate(currPos.getY(), currPos.getX() - 1);
+
+			robotFootprint[BACK_LEFT] = new Coordinate(currPos.getY() + 1, currPos.getX() + 1);
+			robotFootprint[BACK_CENTER] = new Coordinate(currPos.getY() + 1, currPos.getX());
+			robotFootprint[BACK_RIGHT] = new Coordinate(currPos.getY() + 1, currPos.getX() - 1);
+			return robotFootprint;
+		case EAST:
+			robotFootprint[FRONT_LEFT] = new Coordinate(currPos.getY() + 1, currPos.getX() + 1);
+			robotFootprint[FRONT_CENTER] = new Coordinate(currPos.getY(), currPos.getX() + 1);
+			robotFootprint[FRONT_RIGHT] = new Coordinate(currPos.getY() - 1, currPos.getX() + 1);
+
+			robotFootprint[MIDDLE_LEFT] = new Coordinate(currPos.getY() + 1, currPos.getX());
+			robotFootprint[MIDDLE_RIGHT] = new Coordinate(currPos.getY() - 1, currPos.getX());
+
+			robotFootprint[BACK_LEFT] = new Coordinate(currPos.getY() + 1, currPos.getX() - 1);
+			robotFootprint[BACK_CENTER] = new Coordinate(currPos.getY(), currPos.getX() - 1);
+			robotFootprint[BACK_RIGHT] = new Coordinate(currPos.getY() - 1, currPos.getX() - 1);
+			return robotFootprint;
+		case WEST:
+			robotFootprint[FRONT_LEFT] = new Coordinate(currPos.getY() - 1, currPos.getX() - 1);
+			robotFootprint[FRONT_CENTER] = new Coordinate(currPos.getY(), currPos.getX() - 1);
+			robotFootprint[FRONT_RIGHT] = new Coordinate(currPos.getY() + 1, currPos.getX() - 1);
+
+			robotFootprint[MIDDLE_LEFT] = new Coordinate(currPos.getY() - 1, currPos.getX());
+			robotFootprint[MIDDLE_RIGHT] = new Coordinate(currPos.getY() + 1, currPos.getX());
+
+			robotFootprint[BACK_LEFT] = new Coordinate(currPos.getY() - 1, currPos.getX() + 1);
+			robotFootprint[BACK_CENTER] = new Coordinate(currPos.getY(), currPos.getX() + 1);
+			robotFootprint[BACK_RIGHT] = new Coordinate(currPos.getY() + 1, currPos.getX() + 1);
+			return robotFootprint;
+		default:
+			return null;
+		}
+	}
+
+	/**
+	 * Move <tt>Robot</tt> forward by a specified number of steps.
+	 * 
+	 * @param steps
+	 */
+	public void moveForward(int steps) {
+		int newPos;
+		boolean send = false;
+		String warning = "WARNING: moveRobot() is going out of map boundary.";
+
+		switch (currDir) {
+		case NORTH:
+			newPos = currPos.getY() + steps;
+
+			// Prevents Robot from going out of map boundary
+			if (newPos < Map.maxY - 1) {
+				currPos.setY(newPos);
+				send = true;
+			} else
+				System.out.println(warning);
+			break;
+		case SOUTH:
+			newPos = currPos.getY() - steps;
+
+			if (newPos > 0) {
+				currPos.setY(newPos);
+				send = true;
+			} else
+				System.out.println(warning);
+			break;
+		case EAST:
+			newPos = currPos.getX() + steps;
+
+			if (newPos < Map.maxX - 1) {
+				currPos.setX(newPos);
+				send = true;
+			} else
+				System.out.println(warning);
+			break;
+		case WEST:
+			newPos = currPos.getX() - steps;
+
+			if (newPos > 0) {
+				currPos.setX(newPos);
+				send = true;
+			} else
+				System.out.println(warning);
+			break;
+		default: // Do nothing
+		}
+
+	}
+
+	/**
+	 * Rotate <tt>Robot</tt> in the specified direction.
+	 * 
+	 * @param direction
+	 */
+	public void rotate(Rotate direction) {
+		switch (direction) {
+		case RIGHT:	// Rotate clockwise
+			currDir = (currDir + 1) % 4;
+
+	
+		case LEFT:	// Rotate counter-clockwise
+			float newDir = (currDir - 1) % 4;
+
+			// Make it positive as Java will return negative modulus
+			if (newDir < 0)
+				newDir += 4;
+
+			currDir = (int) newDir;
+
+			
+		default: // Do nothing
+		}
+	}
 }
+	
