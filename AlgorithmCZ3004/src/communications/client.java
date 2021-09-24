19c52e92a903860e684b42ed936e9c60f4e02b82
festevil <62298846+festevil@users.noreply.github.com>
2021-09-24 10:15:46 +0800

    Add files via upload

--- AlgorithmCZ3004/src/communications/client.java
+++ AlgorithmCZ3004/src/communications/client.java
@@ -12,10 +12,12 @@ class client {
 	private static Robot realrobot;
 	public static void main(String args[]) throws Exception {
 		realrobot = new Robot(new Coordinate(1,1),2, true);
-		
+		boolean stop;
 		
 		while(true){     
 			try {
+			stop = true;	
+				
 			String str2 = "test";
 			
 			Socket s=new Socket("192.168.15.15", 5001);  // 5001 port number
@@ -27,8 +29,9 @@ class client {
 			
 			str2 = handshake.readLine();
 			
-			System.out.println("Message from Server : " + str2); // Receive from Android ( ADD, X, Y, DIR) FOR OBSTACLE
-                        // OR if add wrongly then remove with (SUB, X, Y, DIR)
+			System.out.println("Message from Server : " + str2); // Receive from Android ( ADD, UID, (X, Y)) FOR OBSTACLE
+                        // OR if add wrongly then remove with (SUB, UID, (X, Y))
+			// ADD FACE (FACE, UID, DIR)
 			while (str2 == null)
 			{
 				handshake.close();
@@ -39,10 +42,17 @@ class client {
 		    System.out.println("Enter Message to client  : "); // Replace this line with algo output
                     // Send intial position (ROBOT, X, Y, DIR), obtained from Robot class
                     // Then send commands e.g. turn right = tr
+		    while (stop){
 		    String msg = myObj.nextLine();  // Read
 		    output.write(msg);  // Send to STM
-
-		    output.flush();  
+		    if (msg == "STM, s") {
+		    	stop = false;
+		    }
+		    output.flush(); 
+		    }
+		    // Send to STM, 'STM, test' commands: w forward, a left, d right, x backward, s stop
+		    // Send to Android, 'AD, test'
+		     
 		    output.close();  
 			
 			s.close(); } 
